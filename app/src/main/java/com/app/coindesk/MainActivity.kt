38d0f5bc7ca744737bc706f4e11fae3e77e15d95
Vasilii Karpenko <invweb@gmail.com>
2021-11-07 08:46:08 +0300

    Add files via upload

--- app/src/main/java/com/app/coindesk/MainActivity.kt
+++ app/src/main/java/com/app/coindesk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.app.coindesk
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -27,6 +30,7 @@ private var rootCoins: Coins? = null
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private var coinsFlag: Boolean = false
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,107 +67,141 @@ class MainActivity : ComponentActivity() {
             }
         })
     }
-}
 
-@Composable
-private fun ShowCoinInfoComposable(navController: NavHostController) {
-    Scaffold(modifier = Modifier.padding(8.dp), content = {
-        Column(modifier = Modifier.padding(0.dp)) {
-            Row {
-                rootCoins?.chartName?.let { it1 ->
+    @Composable
+    private fun ShowCoinInfoComposable(navController: NavHostController) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = getString(R.string.app_name))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            navController.popBackStack()
+                        }) {
+                            Icon(Icons.Filled.ArrowBack, "")
+                        }
+                    },
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White,
+                    elevation = 12.dp
+                )
+
+            },
+            modifier = Modifier.padding(0.dp), content = {
+            Column(modifier = Modifier.padding(0.dp)) {
+                Row {
+                    rootCoins?.chartName?.let { it1 ->
+                        Text(
+                            modifier = Modifier.padding(5.dp),
+                            text = it1
+                        )
+                    }
+                }
+                Row {
                     Text(
                         modifier = Modifier.padding(5.dp),
-                        text = it1
+                        text = (rootCoins?.bpi?.eur?.code)
+                            + " : "
+                            + rootCoins?.bpi?.eur?.rate
+                    )
+                }
+                Row {
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = rootCoins?.bpi?.gbp?.code
+                            + " : "
+                            + rootCoins?.bpi?.gbp?.rate
+                    )
+                }
+                Row {
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = rootCoins?.bpi?.usd?.code
+                            + " : "
+                            + rootCoins?.bpi?.usd?.rate
                     )
                 }
             }
-            Row {
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = (rootCoins?.bpi?.eur?.code)
-                        + " : "
-                        + rootCoins?.bpi?.eur?.rate
-                )
-            }
-            Row {
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = rootCoins?.bpi?.gbp?.code
-                        + " : "
-                        + rootCoins?.bpi?.gbp?.rate
-                )
-            }
-            Row {
-                Text(
-                    modifier = Modifier.padding(5.dp),
-                    text = rootCoins?.bpi?.usd?.code
-                        + " : "
-                        + rootCoins?.bpi?.usd?.rate
-                )
-            }
-        }
-    })
-}
+        })
+    }
 
-@Composable
-fun ShowCoins(navController: NavHostController, coins: List<Coins>) {
-    Scaffold(modifier = Modifier.padding(0.dp), content = {
-        TableComposable(navController, coins)
-    })
-}
+    @Composable
+    fun ShowCoins(navController: NavHostController, coins: List<Coins>) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = getString(R.string.app_name))
+                    },
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White,
+                    elevation = 12.dp
+                )
 
-@Composable
-fun TableComposable(navController: NavHostController, coins: List<Coins>) {
-    Scaffold(modifier = Modifier.padding(0.dp), content = {
-        LazyColumn(
-            modifier = Modifier
-                .padding(0.dp, 0.dp, 0.dp, 0.dp)
-        ) {
-            items(items = coins, itemContent = { coin ->
-                Column(modifier = Modifier.clickable(
-                    onClick = {
-                        rootCoins = coin
-                        navController.navigate("coinInfo")
-                    }
-                )) {
-                    Row {
-                        Text(
-                            modifier = Modifier.padding(5.dp),
-                            text = coin.chartName
-                        )
-                    }
-                    Row {
-                        Text(
-                            modifier = Modifier.padding(5.dp),
-                            text = coin.bpi.eur.code
-                                + " : "
-                                + coin.bpi.eur.rate
-                        )
-                    }
-                    Row {
-                        Text(
-                            modifier = Modifier.padding(5.dp),
-                            text = coin.bpi.gbp.code
-                                + " : "
-                                + coin.bpi.gbp.rate
-                        )
-                    }
-                    Row {
-                        Text(
-                            modifier = Modifier.padding(5.dp),
-                            text = coin.bpi.usd.code
-                                + " : "
-                                + coin.bpi.usd.rate
-                        )
-                    }
-                    Row {
-                        Divider(
-                            color = Color.Black,
-                            thickness = 1.dp
-                        )
-                    }
+            },
+            modifier = Modifier.padding(0.dp),
+            content = {
+            TableComposable(navController, coins)
+        })
+    }
+
+    @Composable
+    fun TableComposable(navController: NavHostController, coins: List<Coins>) {
+        Scaffold(
+            modifier = Modifier.padding(0.dp),
+            content = {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                ) {
+                    items(items = coins, itemContent = { coin ->
+                        Column(modifier = Modifier.clickable(
+                            onClick = {
+                                rootCoins = coin
+                                navController.navigate("coinInfo")
+                            }
+                        )) {
+                            Row {
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = coin.chartName
+                                )
+                            }
+                            Row {
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = coin.bpi.eur.code
+                                        + " : "
+                                        + coin.bpi.eur.rate
+                                )
+                            }
+                            Row {
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = coin.bpi.gbp.code
+                                        + " : "
+                                        + coin.bpi.gbp.rate
+                                )
+                            }
+                            Row {
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = coin.bpi.usd.code
+                                        + " : "
+                                        + coin.bpi.usd.rate
+                                )
+                            }
+                            Row {
+                                Divider(
+                                    color = Color.Black,
+                                    thickness = 1.dp
+                                )
+                            }
+                        }
+                    })
                 }
             })
-        }
-    })
+    }
 }
